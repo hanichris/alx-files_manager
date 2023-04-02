f76ca93713f4ab337979dcb293b987108f00c507
hanichris <hanichris71@gmail.com>
2023-04-02 21:05:11 +0300

    Implement asynchronous functions get, set and del.
    The get, set and del functions are promisified using `utils.promisify`.

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,5 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
@@ -11,6 +12,22 @@ class RedisClient {
 
   isAlive() {
     return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const keyValue = await getAsync(key);
+    return keyValue;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, {EX: duration});
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
   }
 }
 

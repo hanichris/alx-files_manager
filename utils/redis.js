dcf0c1b00e34f976ca2b89c6365fd96babc8f1b2
hanichris <hanichris71@gmail.com>
2023-04-02 20:24:13 +0300

    RedisClient class definition.
    isAlive and constructor methods.

---
+++ utils/redis.js
@@ -0,0 +1,15 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+
+        this.client.on('error', (err) => {
+            console.log(`Redis client not connected to the server: ${err}`);
+        });
+    }
+
+    isAlive() {
+        console.log(`Connected event: ${this.client.connected}`);
+    }
+}

f0bb645a8fc65299e6db1329e5a225d2ce59326d
hanichris <hanichris71@gmail.com>
2023-04-02 21:58:18 +0300

    use if-else block to test for the connection to a redis-server.

--- utils/redis.js
+++ utils/redis.js
@@ -3,35 +3,36 @@ import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.redisIsConnected = undefined;
     this.client = createClient();
 
     this.client.on('error', (err) => {
       this.redisIsConnected = false;
       console.log(`Redis client not connected to the server: ${err}`);
     });
-
-    this.client.on('connect', () => {
-      this.redisIsConnected = true;
-      console.log('Redis client connected to the server');
-    });
   }
 
+  // Check connection to the redis-server.
   isAlive() {
-    return this.redisIsConnected;
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
   }
 
+  // Get the value stored at index 'key' in the redis store.
   async get(key) {
     const getAsync = promisify(this.client.get).bind(this.client);
     const keyValue = await getAsync(key);
     return keyValue;
   }
 
+  // Store a new key: value pair in redis and set an expiration on the key.
   async set(key, value, duration) {
     const setExAsync = promisify(this.client.setex).bind(this.client);
     await setExAsync(key, duration, value);
   }
 
+  // Delete the value stored at index `key` in the redis store.
   async del(key) {
     const delAsync = promisify(this.client.del).bind(this.client);
     await delAsync(key);

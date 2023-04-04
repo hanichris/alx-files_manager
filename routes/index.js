c0e2e5a822b447d5338a404696505a2e76c8bca5
hanichris <hanichris71@gmail.com>
2023-04-05 00:18:44 +0300

    Included three new endpoints: ``GET /users/me`` ``GET /connect`` ``GET /disconnect``

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
-// Status endpoint.
-router.get('/status', AppController.getStatus);
-// Stats endpoint.
-router.get('/stats', AppController.getStats);
-// create new user endpoint
-router.post('/users', UsersController.postNew);
+router.get('/status', AppController.getStatus); // GET /status.
+router.get('/stats', AppController.getStats); // GET /stats.
+router.post('/users', UsersController.postNew); // POST /users.
+router.get('/users/me', UsersController.getMe); // GET /users/me
+router.get('/connect', AuthController.getConnect); // GET /connect
+router.get('/disconnect', AuthController.getDisconnect); // GET /disconnect
 
 module.exports = router;

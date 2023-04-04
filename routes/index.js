695516933a4f346ef34f8c9b929fe91ae386a2bf
hanichris <hanichris71@gmail.com>
2023-04-04 22:06:41 +0300

    create a new endpoint `/users`.
    Enables registration of new users having provided an email and password.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController'
 
 const router = Router();
 
 // Status endpoint.
 router.get('/status', AppController.getStatus);
-
 // Stats endpoint.
 router.get('/stats', AppController.getStats);
+// create new user endpoint
+router.post('/users', UsersController.postNew);
 
 module.exports = router;

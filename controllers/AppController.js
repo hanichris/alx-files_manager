c3de90ff8a7bfc6e1c61673e69d1abf59e3149c9
hanichris <hanichris71@gmail.com>
2023-04-04 13:29:51 +0300

    fix eslint errors semi and object-curly-spacing.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,15 +1,15 @@
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis'
+import redisClient from '../utils/redis';
 
 class AppController {
   static getStatus(request, response) {
-    response.status(200).json({redis: redisClient.isAlive(), db: dbClient.isAlive()});
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static async getStats(request, response) {
     const userCount = await dbClient.nbUsers();
     const fileCount = await dbClient.nbFiles();
-    response.status(200).json({users: userCount, files: fileCount});
+    response.status(200).json({ users: userCount, files: fileCount });
   }
 }
 

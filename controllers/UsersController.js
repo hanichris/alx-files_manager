0dda7599c76f84e53dc8a18190f3023b7a959e2f
hanichris <hanichris71@gmail.com>
2023-04-04 23:18:16 +0300

    register a new user with an email and password.
    First tests if a user with the given email already exists in the db.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,4 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 class UsersController {
@@ -8,12 +9,23 @@ class UsersController {
     if (!email) {
       return response.status(400).json({ error: 'Missing email' });
     }
-
     if (!password) {
       return response.status(400).json({ error: 'Missing password' });
     }
 
-    return response.status(200).json({ msg: 'Success!!!'});
+    (async (userEmail, pwd) => {
+      const user = await dbClient.getUserByEmail(email);
+      if (user) {
+        return response.status(400).json({ error: 'Already exist' });
+      } else {
+        try {
+          const _id = await dbClient.createUser({email: userEmail, password: sha1(pwd)});
+          return response.status(200).json({ id: _id, email: userEmail});
+        } catch (error) {
+          console.error(`Following error occurred: ${error}`);
+        }
+      }
+    })(email, password);
   }
 }
 

22fa080f365101b63741e90044fb92747669b294
hanichris <hanichris71@gmail.com>
2023-04-05 13:41:49 +0300

    Search for a user in the db matching the provided credentials
    The credentials used for a match are the `email` and `password`.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,9 +1,26 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
 class AuthController {
   static getConnect(request, response) {
     const authValue = request.header('Authorization');
     const encodedCredentials = authValue.split(' ')[1];
+    const buff = new Buffer(encodedCredentials, 'base64');
+    const decodedCredentials = buff.toString('ascii');
 
-    return response.status(200).json({ credentials: encodedCredentials });
+    const [ email, pwd ] = decodedCredentials.split(':');
+
+    if (!email || !pwd) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return (async (userEmail, userPwd) => {
+      const user = await dbClient.getUser({ email: userEmail, password: sha1(userPwd) });
+      if (!user) {
+        return response.status(401).json({ error: 'Unauthorized' });
+      }
+      return response.status(200).json({ msg: `Found user with id: ${user.email}`});
+    })(email, pwd);
   }
 }
 

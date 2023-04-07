581b87ee55e86b3b3fa1712609c55623826bfe2a
hanichris <hanichris71@gmail.com>
2023-04-07 19:52:10 +0300

    Define a new endpoint:
    `GET /files/:id/data`

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,6 @@ router.get('/files/:id', FilesController.getShow); // GET /files/:id
 router.get('/files', FilesController.getIndex); // GET /files
 router.put('/files/:id/publish', FilesController.putPublish); // PUT /files/:id/publish
 router.put('/files/:id/unpublish', FilesController.putUnpublish); // PUT /files/:id/unpublish
+router.get('/files/:id/data', FilesController.getFile); // GET /files/:id/data
 
 module.exports = router;

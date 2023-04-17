cff170c82833ca7e0c351462f0df2c385b98da23
hanichris <hanichris71@gmail.com>
2023-04-17 19:13:44 +0300

    Worker to generate thumbnails of image files. Thumbnails' width can be of sizes 500, 250 or 100.

---
+++ worker.js
@@ -0,0 +1,39 @@
+import Queue from 'bull';
+import { writeFile } from 'fs';
+import { ObjectID } from 'mongodb';
+import { imageThumbnail } from 'image-thumbnail';
+import dbClient from './utils/db';
+
+const fileQueue = new Queue('fileQueue', 'redis://127.0.0.1:6379');
+
+fileQueue.process(async (job, done) => {
+  if (!Object.hasOwn(job.data, 'fileId')) {
+    throw new Error('Missing fileId');
+  }
+  if (!Object.hasOwn(job.data, 'userId')) {
+    throw new Error('Missing userId');
+  }
+  const file = await dbClient.getFile({ _id: new ObjectID(job.data.fileId),
+    userId: new ObjectID(job.data.userId) });
+  if (!file) {
+    throw new Error('File not found');
+  }
+
+  const width500Option = { width: 500 };
+  const width250Option = { width: 250 };
+  const width100Option = { width: 100 };
+
+  try {
+    const thumbnailWidth500 = await imageThumbnail(`${file.localPath}`, width500Option);
+    const thumbnailWidth250 = await imageThumbnail(`${file.localPath}`, width250Option);
+    const thumbnailWidth100 = await imageThumbnail(`${file.localPath}`, width100Option);
+
+    writeFile(`${file.localPath}_500`, thumbnailWidth500, (err) => console.error(err));
+    writeFile(`${file.localPath}_250`, thumbnailWidth250, (err) => console.error(err));
+    writeFile(`${file.localPath}_100`, thumbnailWidth100, (err) => console.error(err));
+    done();
+  } catch (e) {
+    console.error(e);
+  }
+
+});
